eebaae75ffbf747d6f3f803027cd52921874580b
Akdev Saha <akdevsaha2003@gmail.com>
2025-07-04 23:08:14 +0530

    feat: backend code for message route

--- api/server/controllers/message.controllers.ts
+++ api/server/controllers/message.controllers.ts
@@ -1,18 +1,45 @@
+import { PrismaClient } from "@prisma/client/extension";
 import { Request, Response } from "express"
 
+
+const prisma = new PrismaClient();
 export const getMessages = async (req: Request, res: Response) => {
-    const { id: userTochatId } = req.params;
+    try {
+        const { id: userTochatId } = req.params;
+        const currentUserId = (req as any).user;
+
+        const messages = await prisma.message.findMany({
+            where: {
+                OR: [
+                    { senderId: currentUserId, receiverId: userTochatId },
+                    { senderId: userTochatId, receiverId: currentUserId }
+                ]
+            },
+            orderBy: {
+                createdAt: 'asc'
+            }
+        });
+
+        res.status(200).json(messages);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get messages", error });
+    }
 }
 
-export const sendMessages = () => {
-    
-}
+export const sendMessages = async (req: Request, res: Response) => {
+    try {
+        const { message, senderId, receiverId } = req.body;
 
-export const getUsers = () => {
+        const newMessage = await prisma.message.create({
+            data: {
+                message,
+                senderId,
+                receiverId
+            }
+        });
 
-}
-
-export const getGroupChats = () => {
-
-
+        res.status(201).json(newMessage);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to send message", error });
+    }
 }
