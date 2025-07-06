36fe95549ea1b47abdab79c06723e96ed6741197
Akdev Saha <akdevsaha2003@gmail.com>
2025-07-06 22:22:01 +0530

    feat(socket): broadcast user online status

--- api/server/index.ts
+++ api/server/index.ts
@@ -3,9 +3,11 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route"
 import messageRoutes from "./routes/message.route"
 import contactRouter from "./routes/contact.route"
+import http from "http"
 import userRouter from "./routes/users.route"
 import chatRouter from "./routes/chat.route"
 import cookieParser from "cookie-parser";
+import { initSocket } from "./socket/socket";
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
@@ -17,6 +19,10 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/contacts", contactRouter);
 app.use("/api/users", userRouter)
 app.use("/api/chat", chatRouter)
-app.listen(port, () => {
+
+const server = http.createServer(app)
+
+initSocket(server)
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
---
+++ api/server/socket/socket.ts
@@ -0,0 +1,65 @@
+import { Server } from "socket.io"
+import { PrismaClient } from "../../generated/prisma";
+import jwt, { JwtPayload } from "jsonwebtoken"
+let io: Server;
+
+const prisma = new PrismaClient();
+export const initSocket = (server: any) => {
+    io = new Server(server, {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST", "DELETE", "PATCH"]
+        }
+    });
+    io.use((socket, next) => {
+        const token = socket.handshake.auth.token;
+        if (!token) return next(new Error("Unauthorized- no token"))
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload
+            socket.data.userId = decoded.userId;
+            next();
+        } catch (error) {
+            console.error("Socket auth failed:", error)
+            return next(new Error("Unauthorized- invalid token"))
+        }
+    });
+
+    io.on("connection", async (socket) => {
+        const userId = socket.data.userId;
+        console.log(`Socket connected: ${socket.id} (userId: ${userId})`)
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                status: 'online'
+            }
+        })
+        socket.broadcast.emit("user-status-updated", {
+            userId,
+            status: "online"
+        })
+        // Handle Disconnect
+        socket.on("disconnect", async () => {
+            console.log(`Socket disconnected: ${socket.id} (userId: ${userId})`)
+            await prisma.user.update({
+                where: {
+                    id: userId
+                },
+                data: {
+                    status: 'offline'
+                }
+            })
+            socket.broadcast.emit("user-status-updated", {
+                userId,
+                status: "offline"
+            })
+        })
+    })
+    return io;
+};
+
+export const getIO = () => {
+    if (!io) throw new Error("Socket.IO not initialized");
+    return io;
+};
